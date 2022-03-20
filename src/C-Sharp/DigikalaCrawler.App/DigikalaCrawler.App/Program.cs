f634282ef285a98eb06cf61ed082227a9e976a0b
Behzad Bagheri <BehzadB@Live.com>
2022-03-20 12:41:44 +0330

    C Sharp

---
+++ src/C-Sharp/DigikalaCrawler.App/DigikalaCrawler.App/Program.cs
@@ -0,0 +1,10 @@
+﻿#region Route Map Project
+// 1.Get Sitemap
+// 2.Read Sitemap sturcture and Get Links from Sitemaps
+// 3.Save Links and other information on database
+// 4.Get WebPage
+// 5.Process and Parse Webpage information
+// 6.Save on a Database
+// 7.Parallel Programing for Distributed
+
+#endregion
